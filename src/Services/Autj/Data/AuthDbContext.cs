da36fa96512588be59207eae8f3f956ba04ff13b
Dave Arnoldi <dave@arnoldi.co.za>
2018-03-23 09:28:26 +0200

    simple JWT working in auth

---
+++ src/Services/Autj/Data/AuthDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            const string databaseName = "Auth";
+            const string databaseUser = "root";
+            const string databasePass = "!IW2bac2821";
+
+            return $"Server=localhost;" +
+                   $"database={databaseName};" +
+                   $"uid={databaseUser};" +
+                   $"pwd={databasePass};" +
+                   $"pooling=true;";
+        }
+    }
+}
